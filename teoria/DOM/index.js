d1e2d7c85ecf1a3f75d2201d6e43caed619062d9
Irissf <iris.seijo@gmail.com>
2022-01-26 12:11:59 +0100

    DOM

---
+++ teoria/DOM/index.js
@@ -0,0 +1,9 @@
+//accedemos al DOM
+console.log(document)
+console.log(document.head)
+console.log(document.title)
+console.log()
+
+//accedemos a las etiquetas
+console.log(document.getElementById("tituloWeb"))
+console.log(document.getElementById("tituloWeb").textContent)
--- teoria/objetos/index.js
+++ teoria/objetos/index.js
@@ -11,8 +11,9 @@ const gato = {
             frio : "salmón",
             caliente: "pollito"
         }
-    },
-    comer : function(){            //métodos
+    //métodos
+    }, 
+    comer : function(){            
         console.log("ñam,ñam");
     }, 
     comerReducido(alimento){
@@ -24,7 +25,15 @@ const gato = {
     },
     ListarEnemigos(){
         this.enemigos.forEach((enemigo) => console.log(enemigo));
+    },
+    //setters getters
+    get nombreMayuscula(){
+        return this.nombre.toLocaleUpperCase();
+    },
+    set agregarEnemigo(nuevoAmigo){
+        this.enemigos.push(nuevoAmigo);
     }
+
 };
 console.log(gato);
 
@@ -57,7 +66,11 @@ gato.comerReducido("pescado");
 gato.comerFecha();
 gato.ListarEnemigos();
 
-//recorrer el objeto con for...in
+//setters y getters
+console.log("nombre en mayúscula: "+gato.nombreMayuscula);
+gato.agregarEnemigo = "Mostachón"; //le mandamos el parámetro con el igual
+
+// ************************ recorrer el objeto con for...in ************************ 
 const perro = {
     nombre:"Valiente Perrón",
     duerme : true,
@@ -68,15 +81,46 @@ for(let propiedad in perro){
     console.log("propiedad: "+ propiedad +" valor: " +perro[propiedad]);
 }
 
-//object.values
+
+
+//************************ object.values ************************ 
 console.log("********** object.values *******");
 console.log(Object.values(perro));
 Object.values(perro).forEach((item) => console.log(item));
 
-//desestructurar objetos
+
+
+// ************************ desestructurar objetos ************************ 
 console.log("********** desestructurar *******");
 //aqui creamos las variables de cada propiedad del objeto, no hace falta poner todas, podemos añadir alias por
 //si el nombre de la variable ya está cogido, podemos si no existe, poner un valor por defecto
 const {nombre: nombrecitoAlias = "valor por defecto si no tiene", duerme, edad, mote = "perraQueen"} = perro;
 console.log(nombrecitoAlias+" mote: "+mote);
 
+//podemos hacerlo con un array
+const amigosArray = ["Cobarde", "Tímido", "Pegajoso"];
+const [aUno, aDos, aTres, aCuatro = "no existe"] = amigosArray;
+console.log("amigo UNO: "+aUno)
+
+// **************************** asignaciones variables **********************
+
+//por valor se pasan los tipos primitivos
+let a = "hola";
+let b = a;
+
+console.log(a);
+a = "adiós"
+
+console.log(b);
+console.log(a);
+
+
+// pero al pasar objetos y arrays, se pasa la referencia, no el valor
+let aArr = ["hola array"];
+let bArr = aArr;
+
+console.log("a " + aArr);
+aArr.push("adiós array");
+
+console.log("a " + aArr);
+console.log("b " + bArr); //cambia los dos, a y b
